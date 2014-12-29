75fc467bcdc2795c7af09c9c8ad5f2fcfe607fc4
SYSTEM <SYSTEM@WIN2012>
2014-12-29 15:01:46 +0400

    commit by emse tools 2014-12-29 15:01:46

---
+++ Scripts/CLUB;!~!~!~!~.js
@@ -0,0 +1,1 @@
+aa.debug("#####################################", aa.env.getValue("EventName"));
---
+++ Scripts/INCLUDES_CUSTOM.js
@@ -0,0 +1,14 @@
+/*------------------------------------------------------------------------------------------------------/
+| Accela Automation
+| Accela, Inc.
+| Copyright (C): 2012
+|
+| Program : INCLUDES_CUSTOM.js
+| Event   : N/A
+|
+| Usage   : Custom Script Include.  Insert custom EMSE Function below and they will be 
+|	    available to all master scripts
+|
+| Notes   :
+|
+/------------------------------------------------------------------------------------------------------*/
